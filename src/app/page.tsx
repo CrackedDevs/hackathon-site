22a478a78e0a4a5015a709bba000864904ade7bb
Anurag30112003 <anuragsharma011011@gmail.com>
2024-01-09 20:58:33 +0530

    ui : hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import { Button } from "@/components/ui/button"
+import SparklesHero from './components/SparklesHero'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-primary">
-     <Button variant="secondary">Button</Button>
+    <main>
+      <SparklesHero/>
     </main>
   )
 }
