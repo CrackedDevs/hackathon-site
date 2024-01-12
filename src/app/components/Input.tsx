6dd3b590bde352d3f6df5ce2ba8860da5830bf99
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2024-01-13 01:22:32 +0530

    fix: input was not controlled through email state
    even though the email state was set to setEmail("") the input was not changing, because the value of the email was uncontrolled.

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -53,6 +53,7 @@ export default function Input() {
           type="email"
           onChange={handleInputChange}
           name="email"
+          value={email}
           autoComplete="email"
           required
           placeholder="elon@spacex.com"
