c2f9182eedc25c1cd60e0a14a13a4a0a05a9e445
Suhas Sumukh <70210383+suhasasumukh@users.noreply.github.com>
2024-02-02 18:23:30 +0530

    added late feb announcment

--- src/app/components/SparklesHero.tsx
+++ src/app/components/SparklesHero.tsx
@@ -7,10 +7,10 @@ export default function SparklesHero() {
     <section className="h-[100vh] relative w-full bg-black flex flex-col items-center justify-center overflow-hidden">
       <div className="absolute w-full h-full z-10 inset-0 bg-[url(/grid.svg)] bg-center bg-cover opacity-30 [mask-image:linear-gradient(180deg,rgba(255,255,255,255))]"></div>
       <p className="text-center text-secondary z-20 text-sm md:text-lg font-bold my-1">
-          Online, January 19 - 21st
+          Online, Late Feb!
         </p>
         <p className="text-center text-secondary z-20 text-sm md:text-lg font-bold my-1">
-          $50,000+ in prizes
+          TBA Soon!
         </p>
       <h1 className="text-4xl sm:text-8xl lg:mb-0 font-bold text-center text-white relative z-20">
         <div className="bg-gradient-to-br from-[#6366F1] to-white text-transparent bg-clip-text">CrackedDevs.com</div> Hackathon
@@ -34,11 +34,11 @@ export default function SparklesHero() {
         {/* Radial Gradient to prevent sharp edges */}
       </div>
       <a
-          href="https://crackeddevs.devpost.com/"
+          href="#"
           target="_blank"
           className="z-40 cursor-pointer rounded-lg capitalize text-lg py-2 px-4 md:px-6 lg:px-8 bg-white text-black"
         >
-          Apply Now!
+          Coming Soon!
         </a>
     </section>
   );
